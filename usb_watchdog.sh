7535b7c397bd2679f921bf106f4716c90f7eb17f
Cristi-Micu <cristian.micu.dev@outlook.com>
2026-03-05 12:37:38 +0200

    Update usb_watchdog.sh

--- usb_watchdog.sh
+++ usb_watchdog.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Follow the kernel log live and look for the specific URB timeout crash
-journalctl -k -f | grep --line-buffered "usb 1-2: timeout" | while read line ; do
+# Follow the kernel log live and look for either crash phrase
+journalctl -k -f | grep --line-buffered -E "usb 1-2: timeout|couldn't find an input interrupt endpoint" | while read line ; do
     # Fire the rescue script to cycle the power
     /usr/local/bin/rescue_keyboard.sh
     # Sleep for 10 seconds so it doesn't trigger multiple times in a row
